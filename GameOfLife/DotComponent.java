9855dd6b80f372f40853a790e310b8467c91ebe7
Keegan Nelson <94640505+Keegan-Nelson@users.noreply.github.com>
2023-01-11 17:43:18 -0700

    Add files via upload

---
+++ GameOfLife/DotComponent.java
@@ -0,0 +1,112 @@
+package gmOfLife;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.util.*;
+import java.awt.geom.*;
+import java.awt.geom.Ellipse2D.Double;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class DotComponent extends JComponent {
+
+		private boolean[][] dots; 
+		private final int gridSize; 
+		private Random gen; 
+		private Color color = Color.black; 
+		
+		
+		public DotComponent() { 
+			super(); 
+			gridSize = 50; 
+			dots = new boolean[gridSize][gridSize];
+			gen = new Random(); 
+			
+			/*for(int row= 0; row < gridSize; row++) { 
+				for(int col=0;col<gridSize; col++) { 
+					dots[row][col]= gen.nextBoolean(); 
+				}
+			
+			}*/
+		}
+		
+		public void nextGen() { 
+			boolean[][] newdots = new boolean[gridSize][gridSize];
+			for( int row = 0; row<gridSize ; row++ ) { 
+				for( int col = 0; col<gridSize; col++) { 
+					int count = 0;
+					if( row-1 >=0 && col-1 >=0) { //&& dots[row-1][col-1]) { 							
+						if(dots[row-1][col-1]) {   //top left 
+							count++; 
+						}	
+					}
+					if( row-1>=0 && dots[row-1][col]) {     //top middle
+						count++;												
+					}
+					if( row-1>=0 && col+1 < gridSize && dots[row-1][col+1]) {	//top right 
+						count++;   	
+					}
+					if( col-1>=0 && dots[row][col-1]) {	// middle left
+						count++; 
+					}
+					if( col+1 < gridSize && dots[row][col+1]) { //middle right					// all of these comments refer to which colum the if statement is pointing at
+						count++; 	
+					}			
+					if( row+1 < gridSize && col-1>=0 && dots[row+1][col-1]) {	//bottom left
+						count++; 
+					}
+					if( row+1 < gridSize && dots[row+1][col]) {  //bottom middle
+						count++;
+					}
+					if( row+1 < gridSize && col+1 < gridSize && dots[row+1][col+1]) {   // bottom right 
+						count++;
+					}
+					
+					if( count == 3) {newdots[row][col]= true;}
+					else if(count ==2) {newdots[row][col]=dots[row][col];} 
+					else {newdots[row][col]=false;} 
+					
+					
+				}
+			}
+		
+			dots = newdots; 
+			repaint();
+		}
+		
+		
+		public void setDot(double x, double y) { 
+			double squareWidth = getWidth() / (double) gridSize; 
+			double squareHeight = getHeight() / (double) gridSize; 
+			
+			int col = (int) (x /squareWidth); 
+			int row = (int) (y /squareHeight); 
+			
+			if( dots [row][col]) {
+				dots[row][col] = false; 
+			}
+			else { 
+				dots[row][col] = true; 
+			}
+			repaint(); 
+		}
+		public void paintComponent(Graphics g) { 
+			super.paintComponent(getGraphics());
+			Graphics2D g2D = (Graphics2D)g; 
+			
+			double squareWidth = getWidth() / (double) gridSize; 
+			double squareHeight = getHeight() / (double) gridSize; 
+			
+			for(int row=0; row < gridSize; row++) { 
+				for(int col=0;col<gridSize; col++) { 
+					
+					if(dots[row][col]) {
+						
+						double x = col * squareWidth; 
+						double y = row * squareHeight; 
+						g2D.fill(new Ellipse2D.Double(x,y,squareWidth,squareHeight));
+					}
+				}
+			}
+		}
+}
---
+++ GameOfLife/DotFrame.java
@@ -0,0 +1,92 @@
+package gmOfLife;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
+
+import javax.swing.*; 
+
+public class DotFrame extends JFrame {
+	private DotComponent dotDisplay; 
+	private Timer timer; 
+	private Random gen; 
+	
+	private JButton step; 
+	private JButton go; 
+	private JButton stop; 
+	
+	private class doTwinkleONCE implements ActionListener{ 
+		public void actionPerformed(ActionEvent ae) { //a
+			dotDisplay.nextGen(); 
+		}
+	}
+	private class doTwinkle implements ActionListener{ 
+		public void actionPerformed(ActionEvent ae) { 
+			// need code to only do TWINKLE ONCE 
+			
+			timer = new Timer(10,new doTwinkleONCE()); 
+			timer.start(); 
+		}
+		
+	}
+	private class stopGM implements ActionListener{ 
+		public void actionPerformed(ActionEvent ae) {
+			
+			//timer = new Timer(10,new doTwinkleONCE()); 
+			timer.stop();
+		}
+	}
+	
+	
+	private class PutDot implements MouseListener{ 
+		public void mouseClicked(MouseEvent arg0) { 
+			double clickX = arg0.getX(); 
+			double clickY = arg0.getY(); 
+			
+			dotDisplay.setDot(clickX,clickY); 
+		}
+		
+		public void mouseEntered(MouseEvent arg0) {}
+		public void mouseExited(MouseEvent arg0) {} 
+		public void mousePressed(MouseEvent arg0) {} 
+		public void mouseReleased(MouseEvent arg0) {} 
+	}
+	
+	
+	public DotFrame() { 
+		super(); 
+		//gen = new Random(); 
+		Container ctPane = getContentPane(); 
+		dotDisplay = new DotComponent(); 
+		dotDisplay.addMouseListener(new PutDot());
+		ctPane.add(dotDisplay, "Center");
+		step = new JButton("Step"); 
+		step.addActionListener(new doTwinkleONCE());           // AcAAction listener that goes through the next gen once 
+		go = new JButton("Play");
+		go.addActionListener(new doTwinkle());
+		stop = new JButton("Stop"); 
+		stop.addActionListener(new stopGM()); 
+		
+		JPanel bottomPanel = new JPanel(); 
+		bottomPanel.add(stop); 
+		bottomPanel.add(go); 
+		bottomPanel.add(step); 
+		ctPane.add(bottomPanel,BorderLayout.SOUTH);
+		//timer = new Timer(10,new doTwinkle()); 
+		//timer.start(); 
+	
+	}
+	
+	public DotFrame( String title) { 
+		this(); 
+		setTitle(title); 
+		
+	}
+	
+	public static void main(String[] args) { 
+		DotFrame frame = new DotFrame("Game of Life"); 
+		frame.setSize(500,500); 
+		frame.setLocation(200,100);
+		frame.setVisible(true); 
+		frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+	}
+}
