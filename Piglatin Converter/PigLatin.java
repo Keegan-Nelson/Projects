76a5d92d7717b350f222bcda6c9fb9999f2097cd
Keegan Nelson <94640505+Keegan-Nelson@users.noreply.github.com>
2023-02-16 16:38:11 -0700

    Update PigLatin.java

--- Piglatin Converter/PigLatin.java
+++ Piglatin Converter/PigLatin.java
@@ -5,9 +5,9 @@ public class PigLatin {
 		
 		word = word.toLowerCase();
 		
-		int firstvowel=-1;							// maybe want it at negative 1   (-1) 
+		int firstvowel=-1;							
 		for(int i=0; i<word.length(); i++) {
-			char letter = word.charAt(i);        // This will go through the word and then find out what the first vowel is. 									
+			char letter = word.charAt(i);       
 			
 			if(		letter =='a' || letter =='e' || letter =='i' || 
 					letter =='o' || letter =='u'				)
@@ -15,7 +15,7 @@ public class PigLatin {
 					firstvowel = i; 
 					break;
 				}
-			if(letter =='y' && i!= 0) {				// this is a different set of code of the word doesnt't have a vowel like Rhythm
+			if(letter =='y' && i!= 0) {			
 				firstvowel = i;
 				break;
 			}
@@ -29,14 +29,13 @@ public class PigLatin {
 		
 		if(firstvowel==0) {
 			Pigword = word + "-pay";
-		}																// this is the part of repalcing the beginning of the word with pay or ay at the end of the word
+		}								// this is the part of repalcing the beginning of the word with pay or ay at the end of the word
 		else {
 			Pigword = restOfWord +"-"+consonants+"ay"; 
 		}
 		
 		return Pigword; 						//this returns the translated word. within the tokenizer it will go through each token defaultly spaced out by a "space" 
-		//System.out.println( firstvowel ); 	//after each word is converted it is stored and when all the words are converted it displays below the orignizal text. 
-		//return null;
+										//after each word is converted it is stored and when all the words are converted it displays below the orignizal text. 
 		
 	}
 
